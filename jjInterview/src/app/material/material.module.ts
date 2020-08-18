9f070b604d642d2097152e86f77a1a79e36e2651
Courtney Revada <revadacourtney@gmail.com>
2020-08-17 20:43:59 -0700

    chore: added mat-table and mat-slider

--- jjInterview/src/app/material/material.module.ts
+++ jjInterview/src/app/material/material.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSliderModule,
+    MatTableModule
+  ],
+  exports: [
+    MatSliderModule,
+    MatTableModule
   ]
 })
 export class MaterialModule { }
